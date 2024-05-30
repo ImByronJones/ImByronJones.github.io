63dd4c3ecea7a570ae15c9f3f88f9c259f620d51
Byron Jones <116259277+SupaBroon@users.noreply.github.com>
2024-05-29 23:35:48 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+function replayVideo(video) {
+  video.currentTime = 0;
+  video.play();
+}
+
+function toggleMute(video, button) {
+  if(video.muted) {
+    video.muted = false;
+    button.textContent = "Mute";
+  } else {
+    video.muted = true;
+    button.textContent = "Unmute";
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  var replayButton = document.getElementById('proj-2-replay');
+  var video2 = document.getElementById('proj-2');
+
+  replayButton.addEventListener('click', function() {
+    replayVideo(video2);
+  });
+
+  var replayButton2 = document.getElementById('proj-1-replay');
+  var video1 = document.getElementById('proj-1');
+
+  replayButton2.addEventListener('click', function() {
+    replayVideo(video1);
+  });
+
+  var replayButton3 = document.getElementById('proj-3-replay');
+  var video3 = document.getElementById('proj-3');
+
+  replayButton3.addEventListener('click', function() {
+    replayVideo(video3);
+  })
+
+  var muteButton = document.getElementById('mute');
+  
+  muteButton.addEventListener('click', function() {
+    toggleMute(video2, muteButton);
+  })
+});
