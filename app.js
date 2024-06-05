647845208947e618101e0e1f8756a0ce626838c5
Byron Jones <byronjones400@gmail.com>
2024-06-05 15:36:53 -0700

    fixed pages

--- app.js
+++ app.js
@@ -14,35 +14,35 @@ function toggleMute(video, button) {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-  var replayButton = document.getElementById('proj-2-replay');
-  var video2 = document.getElementById('proj-2');
+  // var replayButton = document.getElementById('proj-2-replay');
 
-  replayButton.addEventListener('click', function() {
-    replayVideo(video2);
-  });
+  // replayButton.addEventListener('click', function() {
+  //   replayVideo(video2);
+  // });
 
-  var replayButton2 = document.getElementById('proj-1-replay');
-  var video1 = document.getElementById('proj-1');
+  // var replayButton2 = document.getElementById('proj-1-replay');
+  // var video1 = document.getElementById('proj-1');
 
-  replayButton2.addEventListener('click', function() {
-    replayVideo(video1);
-  });
+  // replayButton2.addEventListener('click', function() {
+  //   replayVideo(video1);
+  // });
 
-  var replayButton3 = document.getElementById('proj-3-replay');
-  var video3 = document.getElementById('proj-3');
+  // var replayButton3 = document.getElementById('proj-3-replay');
+  // var video3 = document.getElementById('proj-3');
 
-  replayButton3.addEventListener('click', function() {
-    replayVideo(video3);
-  })
+  // replayButton3.addEventListener('click', function() {
+  //   replayVideo(video3);
+  // })
 
-  var replayButton4 = document.getElementById('proj-4-replay');
-  var video4 = document.getElementById('proj-4');
+  // var replayButton4 = document.getElementById('proj-4-replay');
+  // var video4 = document.getElementById('proj-4');
 
-  replayButton4.addEventListener('click', function() {
-    replayVideo(video4);
-  })
+  // replayButton4.addEventListener('click', function() {
+  //   replayVideo(video4);
+  // })
 
   var muteButton = document.getElementById('mute');
+  var video2 = document.getElementById('proj-2');
   
   muteButton.addEventListener('click', function() {
     toggleMute(video2, muteButton);
